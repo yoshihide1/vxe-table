6f952f41355445c7865f02dbbebdc56275696fb3
yoshihide <aqua.sky.33333@gmail.com>
2020-07-06 21:18:55 +0900

    不要になったファイルの削除

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">User</router-link> |
-      <router-link to="/about">API</router-link> |
-      <router-link to="/corona">コロナ</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -16,18 +11,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
