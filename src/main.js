0729de0dba5b2a5f71bdf7b696bba15d5339830b
yoshihide <aqua.sky.33333@gmail.com>
2020-07-08 21:25:55 +0900

    vxetable削除

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'xe-utils'
-import VXETable from 'vxe-table'
-import 'vxe-table/lib/index.css'
 
 import 'chart.js'
 
@@ -13,9 +10,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(VXETable)
-
-Vue.prototype.$modal = VXETable.modal
 Vue.config.productionTip = false
 
 new Vue({
