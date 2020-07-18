a90a2b60253ec12601ac1293534694bb2a4210fc
yoshihide <aqua.sky.33333@gmail.com>
2020-07-18 10:17:20 +0900

    都道府県別の部分コンポーネント化

--- src/components/DomesticPrefecture.vue
+++ src/components/DomesticPrefecture.vue
@@ -1,15 +1,74 @@
 <template>
   <div>
-    DomesticPrefecture
+    <div class="card">
+      <div class="card__title">
+        <p>
+          <select v-model="selected">
+            <option v-for="pref in prefs" :key="pref.code" :value="pref.code">{{ pref.name }}</option>
+          </select>
+          <span class="card__font__span">(現在)</span>
+        </p>
+      </div>
+      <div class="card__body">
+        <p>
+          <span class="card__font__span">感染者</span>
+          ：{{ numComma(prefNowCase) }}
+          <span class="card__font__span">人</span>
+        </p>
+        <p>
+          <span class="card__font__span">{{ prefName }}人口の</span>
+          {{ prefNowPercentage }}%
+        </p>
+        <p>
+          <span class="card__font__span">{{ prefName }}の人口:{{ prefPopulation }}人</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
+import prefs from "../assets/prefectures.json";
 export default {
-
-}
+  data() {
+    return {
+      prefs: prefs,
+      selected: 13,
+      cases: 0,
+      prefNowCase: 0,
+      prefNowPercentage: 0,
+      prefPopulation: 0,
+      prefName: ""
+    };
+  },
+  computed: {
+    ...mapState(["coronaData", "coronaPrefData"]),
+    ...mapGetters(["prefDataFilter", "numComma"])
+  },
+  watch: {
+    selected() {
+      this.$store.commit("prefTotal", this.prefDataFilter(this.selected));
+    },
+    coronaData() {
+      this.$store.commit("prefTotal", this.prefDataFilter(this.selected));
+    },
+    coronaPrefData() {
+      this.prefFilter(this.coronaPrefData);
+    }
+  },
+  methods: {
+    prefFilter(pref) {
+      let now = pref.cases - pref.discharge - pref.deaths;
+      let percentage = (now / pref.population) * 100;
+      this.prefNowCase = now;
+      this.prefNowPercentage = Math.floor(percentage * 100000) / 100000;
+      this.prefName = pref.prefecture;
+      this.prefPopulation = this.numComma(pref.population);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
