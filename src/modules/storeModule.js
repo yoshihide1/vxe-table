a3a533b0d5b74195db8f449679d30d5e1c7b5eaa
yoshihide <aqua.sky.33333@gmail.com>
2020-09-26 21:11:05 +0900

    stateのデータ追加

--- src/modules/storeModule.js
+++ src/modules/storeModule.js
@@ -44,29 +44,53 @@ export const state = {
   updateChart: [],
   ratio: [
     {
-      pref_id: "13",
-      prefecture: "東京都",
-      cases: "23828",
-      population: "13942856",
-      deaths: "390",
-      pcr: "408414",
-      hospitalize: "2230",
-      severe: "26",
-      discharge: "21208",
-      created_at: "20200919"
-    },
-    {
-      pref_id: "13",
-      prefecture: "東京都",
-      cases: "24046",
-      population: "13942856",
-      deaths: "391",
-      pcr: "411359",
-      hospitalize: "2278",
-      severe: "25",
-      discharge: "21378",
-      created_at: "20200920"
-    },
+      "pref_id": "13",
+      "population": "13942856",
+      "cases": "23828",
+      "deaths": "390",
+      "pcr": "408414",
+      "hospitalize": "2230",
+      "severe": "26",
+      "discharge": "21208",
+      "created_at": "20200919",
+      "prefecture": "東京都"
+  },
+  {
+      "pref_id": "13",
+      "population": "13942856",
+      "cases": "24046",
+      "deaths": "391",
+      "pcr": "411359",
+      "hospitalize": "2278",
+      "severe": "25",
+      "discharge": "21378",
+      "created_at": "20200920",
+      "prefecture": "東京都"
+  },
+  {
+      "pref_id": "13",
+      "population": "13942856",
+      "cases": "24208",
+      "deaths": "390",
+      "pcr": "411359",
+      "hospitalize": "2346",
+      "severe": "27",
+      "discharge": "21472",
+      "created_at": "20200921",
+      "prefecture": "東京都"
+  },
+  {
+      "pref_id": "13",
+      "population": "13942856",
+      "cases": "24306",
+      "deaths": "391",
+      "pcr": "411359",
+      "hospitalize": "2375",
+      "severe": "27",
+      "discharge": "21540",
+      "created_at": "20200922",
+      "prefecture": "東京都"
+  },
   ],
   byPrefData: [
     {
