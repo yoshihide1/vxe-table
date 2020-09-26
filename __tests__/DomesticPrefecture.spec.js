c911a57398abbdc8e93b8479d01504d7f43b5170
yoshihide <aqua.sky.33333@gmail.com>
2020-09-26 17:00:32 +0900

    DomesticPrefecture.vueのテスト追加

---
+++ __tests__/DomesticPrefecture.spec.js
@@ -0,0 +1,36 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import { getters } from '@/store/index.js'
+import { state } from '@/modules/storeModule.js'
+import DomesticPrefecture from '@/components/DomesticPrefecture.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('DomesticPrefecture.vue', () => {
+  let store
+  let wrapper
+  let vm
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state,
+      getters
+    })
+    wrapper = shallowMount(DomesticPrefecture, { store, localVue })
+    vm = wrapper.vm
+  })
+  it('method-prefFilter', () => {
+    vm.prefFilter(vm.$store.state.prefData, 'test')
+    expect(vm.prefNowCase).toBe('2,667')
+    expect(vm.prefNowPercentage).toBe(0.01912)
+    expect(vm.prefName).toBe('東京都')
+    expect(vm.prefPopulation).toBe('13,942,856')
+  })
+  it('method-check', () => {
+    const value = vm.$store.state.prefData
+    const result = `+${value.cases.today - value.cases.yesterday}`
+    vm.check(result, 'test')
+    expect(vm.comparisonCases).toBe('+218')
+  })
+})
--- src/components/DomesticPrefecture.vue
+++ src/components/DomesticPrefecture.vue
@@ -12,7 +12,9 @@
       >
         <span class="py-4 header__title">
           <select v-model="selected">
-            <option v-for="pref in prefs" :key="pref.code" :value="pref.code">{{ pref.name }}</option>
+            <option v-for="pref in prefs" :key="pref.code" :value="pref.code">
+              {{ pref.name }}
+            </option>
           </select>
         </span>
         <span class="header__title__sub">(現在)</span>
@@ -55,7 +57,8 @@ export default {
     },
   },
   methods: {
-    prefFilter(pref) {
+    //test>test用の引数
+    prefFilter(pref, test = "") {
       const a = pref["cases"].today - pref["discharge"].today;
       const b = pref["deaths"].today - pref["deaths"].yesterday;
       const now = a - b;
@@ -65,12 +68,14 @@ export default {
       this.prefNowPercentage = Math.floor(percentage * 100000) / 100000;
       this.prefName = pref["prefecture"];
       this.prefPopulation = this.numComma(pref["population"].today);
+      if (test) return; //test時ここで止める為
       this.check(prefNow);
     },
-    check(data) {
-      const d = this.$refs.case.$el;
+    check(data, test = "") {
       this.comparisonCases = data;
-      if (data > 0) {
+      if (test) return;
+      const d = this.$refs.case.$el;
+      if (this.comparisonCases > 0) {
         d.classList.replace("case__minus", "case__plus");
       } else {
         d.classList.replace("case__plus", "case__minus");
