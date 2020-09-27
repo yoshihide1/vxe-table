c1cc5daf31c5caeb0396ba97f7d7b3c33d101181
yoshihide <aqua.sky.33333@gmail.com>
2020-09-27 10:53:08 +0900

    LineChart.vueのテスト追加

--- __tests__/Chart.spec.js
+++ __tests__/Chart.spec.js
@@ -1,7 +1,10 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils'
 import Vuex from 'vuex'
 import { state } from '@/modules/storeModule.js'
+import { getters } from '@/store/index.js'
 import LineChartPref from '@/components/LineChartPref.vue'
+import LineChartPrefPcr from '@/components/LineChartPrefPcr.vue'
+import LineChart from '@/components/LineChart.vue'
 
 
 const localVue = createLocalVue()
@@ -11,24 +14,64 @@ let store
 let wrapper
 let vm
 
-beforeEach(() => {
-  store = new Vuex.Store({
-    state
+describe('Chart', () => {
+
+  describe('LineChartPref.vue', () => {
+    beforeEach(() => {
+      store = new Vuex.Store({
+        state,
+        getters
+      })
+      wrapper = shallowMount(LineChartPref, { store, localVue })
+      vm = wrapper.vm
+    })
+    it('method-setPref', () => {
+      vm.setPref(true)
+      expect(vm.date).toStrictEqual(["09月20日", "09月21日", "09月22日", "09月23日"])
+      expect(vm.cases).toStrictEqual([40, 31, 33, 19])
+      expect(vm.discharge).toStrictEqual([30, 14, 14, 21])
+      expect(vm.hospitalize).toStrictEqual([9, 16, 17, -2])
+    })
   })
-  wrapper = shallowMount(LineChartPref, { store, localVue })
-  vm = wrapper.vm
-})
-describe('LineChartPref.vue', () => {
+
+  describe('LineChartPrefPcr.vue', () => {
+    beforeEach(() => {
+      store = new Vuex.Store({
+        state,
+        getters
+      })
+      wrapper = shallowMount(LineChartPrefPcr, { store, localVue })
+      vm = wrapper.vm
+    })
+    it('method-setPref', () => {
+      vm.setPref(true)
+      expect(vm.date).toStrictEqual(["09月20日", "09月21日", "09月22日", "09月23日"])
+      expect(vm.pcr).toStrictEqual([900, 777, 259, 279])
+    })
+  })
+
+  describe('LineChart.vue', () => {
+    let wrapper
+    let vm
+    beforeEach(() => {
+      store = new Vuex.Store({
+        state,
+        getters
+      })
+      wrapper = shallowMount(LineChart, { store, localVue })
+      vm = wrapper.vm
+    })
+    it('method-setRatio', () => {
+      vm.setRatio(true)
+      expect(vm.cases).toStrictEqual([218, 162, 98])
+      expect(vm.discharge).toStrictEqual([170, 94, 68])
+      expect(vm.hospitalize).toStrictEqual([48, 68, 29])
+      expect(vm.date).toStrictEqual(["09月20日", "09月21日", "09月22日"])
 
 
-  it('method-setPref-chartData', () => {
-    vm.setPref('test')
-    expect(vm.date).toStrictEqual(["09月20日", "09月21日", "09月22日", "09月23日"])
-    expect(vm.cases).toStrictEqual([40, 31, 33, 19])
-    expect(vm.discharge).toStrictEqual([30, 14, 14, 21])
-    expect(vm.hospitalize).toStrictEqual([9, 16, 17, -2])
-  })
-  it('method-dateFormat', () => {
-    expect(vm.dateFormat("20200923")).toBe("09月23日")
+    })
+    it('method-dateFormat', () => {
+      expect(vm.dateFormat("2020-10-20")).toBe('10月20日')
+    })
   })
 })
